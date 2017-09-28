2973fc0028d467eba5e96666cc5a74db14ad0315
Rachel Richardson <rachjr@umich.edu>
2017-09-28 16:01:31 -0600

    Updated lab4.py

---
+++ lab4.py
@@ -0,0 +1,10 @@
+import re
+with open('mbox-short.txt') as f:
+    f = f.readlines()
+    for line in f:
+        if re.search('From', line):
+            print (line)
+            numbers = re.findall('[0-9]+', line)
+            print(numbers)
+            name = re.findall('(\S*)@', line)
+            print(name)
